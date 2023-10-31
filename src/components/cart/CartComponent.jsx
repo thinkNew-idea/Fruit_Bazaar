9b7d57fe52c423e95fbb6672a9fcdc871fc7648d
Aman Jaiswal <aman.jaiswal@indrones.com>
2023-10-31 23:16:51 +0530

    working on cart pge

--- src/components/cart/CartComponent.jsx
+++ src/components/cart/CartComponent.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const CartComponent = () => {
-    const cartItems = useSelector(state =>state.cart.cartItems);
+    const cartItems = useSelector(state => state.cart.cartItems);
     return (
         <div>
             {cartItems.map((item, index) => (
                 <div key={index}>
+                    <img src={`${item.product_image}`} />
                     <div>Name: {item.product_name}</div>
                     <div>Size: {item.size}</div>
                     <div>Sale Price: {item.sale_price}</div>
