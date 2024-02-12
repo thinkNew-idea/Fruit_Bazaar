edff9842ef84bf46b2a16181a5f84327dda65f8b
Aman Jaiswal <aman.jaiswal@indrones.com>
2024-02-12 10:44:27 +0530

    login page

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BaseUrl } from "./common";
-
-const ApiCall = async (type, endUrl, requestData) => {
+// verify email api start
+export const ApiCall = async (type, endUrl, requestData) => {
   let header = {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -25,5 +25,33 @@ const ApiCall = async (type, endUrl, requestData) => {
       .catch((error) => reject(error));
   });
 };
+// verify email api end
 
-export default ApiCall;
+// login api start
+export const Loginapicall = async (type, endUrl, requestData) => {
+  let header = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+  const token = localStorage.getItem("token");
+  if (token) {
+    header["Authorization"] = `Bearer ${token}`;
+  }
+  let config = {
+    method: type,
+    maxBodyLength: Infinity,
+    url: BaseUrl + endUrl,
+    headers: header,
+    data: JSON.stringify(requestData),
+  };
+
+  return new Promise((resolve, reject) => {
+    axios
+      .request(config)
+      .then((response) => resolve(response))
+      .catch((error) => reject(error));
+  });
+}
+// login api end
+
+
